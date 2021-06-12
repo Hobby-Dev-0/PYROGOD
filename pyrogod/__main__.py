39cd0718766c85fae35b1ac72961401e39e88e6d
GODBOYX <77817462+GODBOYX@users.noreply.github.com>
2021-06-12 12:10:21 +0530

    Create __main__.py

---
+++ pyrogod/__main__.py
@@ -0,0 +1,9 @@
+# (C) 2021 Paramendra Singh
+
+from pyrogram import idle, Client, filters
+from pyrogod import app, LOGGER
+from pyrogod.modules import *
+import os
+
+print("PyroGod Started \nPowered By @Perry_XD\n(C) 2021 Paramedra Singh")
+app.run()
