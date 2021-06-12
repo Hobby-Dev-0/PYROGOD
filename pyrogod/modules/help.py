5ef8b771d65dc93dd1dfcf8b6ad8e268bfb38c88
GODBOYX <77817462+GODBOYX@users.noreply.github.com>
2021-06-12 12:15:56 +0530

    Add files via upload

---
+++ pyrogod/modules/help.py
@@ -0,0 +1,35 @@
+
+# (C) 2021 Paramendra Singh
+
+from pyrogram import filters
+from pyrogod import app, HELP, CMD_HELP
+from config import PREFIX
+from pyrogod.helpers.pyrohelper import get_arg
+
+HELP.update(
+    {
+        "**Admin Tools**": "ban, unban, promote, demote, kick, mute, unmute, pin, purge, del, invite",
+        "**Alive**": "alive, ping",
+        "**Developer**": "eval sh",
+        "**Misc**": "paste, tr, info, id",
+        "**Heroku**": "update, restart, logs",
+    }
+)
+
+
+@app.on_message(filters.command("help", PREFIX) & filters.me)
+async def help(client, message):
+    args = get_arg(message)
+    if not args:
+        text = "**Available Commands**\n\n"
+        for key, value in HELP.items():
+            text += f"{key}: {value}\n\n"
+        await message.edit(text)
+        return
+    else:
+        module_help = CMD_HELP.get(args, False)
+        if not module_help:
+            await message.edit("Invalid module name specified")
+            return
+        else:
+            await message.edit(module_help)
