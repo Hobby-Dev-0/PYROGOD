92efb062981d970bdd23442bf731c5463f75bccc
GODBOYX <77817462+GODBOYX@users.noreply.github.com>
2021-06-12 14:29:49 +0530

    Update help.py

--- pyrogod/modules/help.py
+++ pyrogod/modules/help.py
@@ -8,11 +8,17 @@ from pyrogod.helpers.pyrohelper import get_arg
 
 HELP.update(
     {
-        "**Admin Tools**": "ban, unban, promote, demote, kick, mute, unmute, pin, purge, del, invite",
-        "**Alive**": "alive, ping",
-        "**Developer**": "eval sh",
-        "**Misc**": "paste, tr, info, id",
-        "**Heroku**": "update, restart, logs",
+        "**Admin Tools**": "__ban, unban, promote, demote, kick, mute, unmute, gmute, ungmute, pin, purge, del, invite__",
+        "**AFK**": "__afk, unafk__",
+        "**Alive**": "__alive, ping__",
+        "**Developer**": "__eval, term__",
+        "**Filters**": "__filter, filters, stop, stopall__",
+        "**Misc**": "__paste, tr, whois, id__",
+        "**Notes**": "__save, get, clear, clearall, notes__",
+        "**Anti-PM**": "__pmguard, setpmmsg, setlimit, setblockmsg, allow, deny__",
+        "**Sticker**": "__kang, stkrinfo__",
+        "**Greetings**": "__setwelcome, clearwelcome__",
+        "**Updater**": "__update__",
     }
 )
 
@@ -29,7 +35,7 @@ async def help(client, message):
     else:
         module_help = CMD_HELP.get(args, False)
         if not module_help:
-            await message.edit("Invalid module name specified")
+            await message.edit("__Invalid module name specified.__")
             return
         else:
             await message.edit(module_help)
