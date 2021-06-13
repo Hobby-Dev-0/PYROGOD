d4b3380ab1d528b66d0035756bd0e3be81b3dc3f
GODBOYX <77817462+GODBOYX@users.noreply.github.com>
2021-06-13 12:32:48 +0530

    Update __init__.py

--- pyrogod/__init__.py
+++ pyrogod/__init__.py
@@ -10,8 +10,9 @@ logging.basicConfig(
     level=logging.DEBUG,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 )
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-LOGGER = logging.getLogger("pyrogram").setLevel(logging.WARNING)
+LOGGER = logging.getLogger(__name__)
 
 
 HELP = {}
